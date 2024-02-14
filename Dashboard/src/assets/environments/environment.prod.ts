c933f2edaa2d3781b2581d9e3694c7e90fd06889
Mutnalsaksha <mutnalsaksha@gmail.com>
2024-02-14 22:14:54 +0530

    Remove Dashboard

--- Dashboard/src/assets/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-
-export const environment={
-    production:true,
-    baseURL:"http://abhi:5000/api/data"
-}
--- Dashboard/src/assets/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-
-export const environment={
-    production:false,
-    baseURL:"http://abhi:5000/api/data"
-}
